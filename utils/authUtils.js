f51aa9f9c67a3f2d04b4d28f3fff68c700368c0d
OgheneDev <emmanueloghene72@gmail.com>
2025-04-25 23:43:15 +0100

    set token

--- utils/authUtils.js
+++ utils/authUtils.js
@@ -5,6 +5,17 @@ export const sendTokenResponse = (user, statusCode, res) => {
         expiresIn: process.env.JWT_EXPIRE,
     });
 
+    // Cookie options
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production'
+    };
+
+    res.cookie('token', token, cookieOptions);
+    
     res.status(statusCode).json({
         success: true,
         token,
